a8f814ccdbe89867077e39008d6c9e255fda564f
Yitbarek1 <Yitbarekdegemu@gmail.com>
2022-04-12 13:47:17 -0700

    Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,6 +10,7 @@ int main(void)
 {
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
+
 	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
@@ -18,7 +19,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 
-		if (count ==49)
+		if (count == 49)
 			printf("\n");
 		else
 			printf(", ");
