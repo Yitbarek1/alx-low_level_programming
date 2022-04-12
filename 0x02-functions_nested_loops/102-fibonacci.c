ecce65aff442728a718191acc3a6eaa0c1e03626
Yitbarek1 <Yitbarekdegemu@gmail.com>
2022-04-12 13:27:31 -0700

    int main(void)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+	assigned long fib1 = 0, fib2 = 1, sum;
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%d", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count ==49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
