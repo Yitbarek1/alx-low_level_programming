c0364aa2d8d57cf6e44b7837e0594d96b28d99cf
Yitbarek1 <Yitbarekdegemu@gmail.com>
2022-04-14 04:32:33 -0700

    first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+nclude "holberton.h"
 
 /**
  * _isupper - Check if a letter is upper
@@ -8,10 +8,10 @@
  */
 int _isupper(int x)
 {
-if (x >= 65 && x <= 90)
-{
-return(1);
-}
-return(0);
-}
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
 
+	return (0);
+}
