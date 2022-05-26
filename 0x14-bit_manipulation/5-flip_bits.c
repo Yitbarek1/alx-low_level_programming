d605889084dd1252cea387b2ef0dfc61ccccf01e
Yitbarek1 <Yitbarekdegemu@gmail.com>
2022-05-26 12:37:50 -0700

    correcting error

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,29 +1,24 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * flip_bits - Write a function that returns the number of bits you would
- * need to flip to get from one number to another.
- * Prototype: unsigned int flip_bits(unsigned long int n, unsigned long int m);
- * You are not allowed to use the % or / operators
- * @n: Number Input 1.
- * @m: Number Input 2.
- * Return: Number of flip in the Input numbers.
+ * flip_bits - number of bits you would need to flip
+ * to get from one number to another
+ * @n: parameter
+ * @m: destiny
+ * Return: nflips
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-
 {
-	unsigned int flip = 0, comp, comp1;
+	unsigned int i, nflips = 0;
+	unsigned long int j = sizeof(unsigned long int) * 8;
 
-	while (!(n == 0 && m == 0))
+	for (i = 0; i < j; i++)
 	{
-		comp = n & 1;
-		comp1 = m & 1;
+		if ((m & 1) != (n & 1))
+			nflips += 1;
 		n = n >> 1;
 		m = m >> 1;
-		if (comp != comp1)
-			flip += 1;
 	}
-	return (flip);
+	return (nflips);
 }
 
